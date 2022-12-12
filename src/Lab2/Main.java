990abff05e52874978d49001c058e321360d554a
Yulia <lavrinenkoll2003@gmail.com>
2022-12-12 15:57:43 +0200

    added the ability to enter different dimensions of matrices

--- src/Lab2/Main.java
+++ src/Lab2/Main.java
@@ -2,8 +2,8 @@ package Lab2;
 import java.util.Scanner;
 import java.util.Random;
 /**
- C5 = 314 mod 5 = 4     C=A×B
- C7 = 314 mod 7 = 6     float
+ C5 = 314 mod 5 = 4     C=A×B - дія, що виконується з матрицями
+ C7 = 314 mod 7 = 6     float - тип елементів матриці
  С11 = 314 mod 11 = 6   Обчислити суму найбільших елементів в стовпцях
  матриці з парними номерами та найменших елементів в стовпцях матриці з непарними номерами
  */
@@ -12,74 +12,116 @@ public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
         Scanner in = new Scanner(System.in);
-
-        String n_str = "";
-        int n = 0;
-        while (n <= 0) {
-            System.out.print("Введіть розмірність матриці (більше за нуль): ");
-            n_str = in.nextLine();
-            try {
-                n = Integer.parseInt(n_str);
-            } catch (NumberFormatException e) {
-                System.out.println("Помилка вводу: введіть натуральне число.");
+        int m = -1, n2 = -2, n = 0, m2 = 0;
+        //введення розмірності 2 матриць
+        while (m != n2) {
+            System.out.println("Введіть розмірність першої матриці: ");
+            System.out.print("Введіть кількість рядків: ");
+            String str = in.nextLine();
+            while (!str.matches("[0-9]+") || str.matches("0")) {
+                System.out.println("Помилка: введіть число, більше 0.");
+                System.out.print("Введіть кількість рядків: ");
+                str = in.nextLine();
+            }
+            n = Integer.parseInt(str);
+            System.out.print("Введіть кількість стовпців: ");
+            str = in.nextLine();
+            while (!str.matches("[0-9]+") || str.matches("0")) {
+                System.out.println("Помилка: введіть число, більше 0.");
+                System.out.print("Введіть кількість стовпців: ");
+                str = in.nextLine();
+            }
+            m = Integer.parseInt(str);
+            System.out.println("\nВведіть розмірність другої матриці: ");
+            System.out.print("Введіть кількість рядків: ");
+            str = in.nextLine();
+            while (!str.matches("[0-9]+") || str.matches("0")) {
+                System.out.println("Помилка: введіть число, більше 0.");
+                System.out.print("Введіть кількість рядків: ");
+                str = in.nextLine();
+            }
+            n2 = Integer.parseInt(str);
+            System.out.print("Введіть кількість стовпців: ");
+            str = in.nextLine();
+            while (!str.matches("[0-9]+") || str.matches("0")) {
+                System.out.println("Помилка: введіть число, більше 0.");
+                System.out.print("Введіть кількість стовпців: ");
+                str = in.nextLine();
+            }
+            m2 = Integer.parseInt(str);
+            if (m != n2) {
+                System.out.println("Помилка: кількість стовпців першої матриці не дорівнює кількості рядків другої матриці.");
+                System.out.println("Множення неможливе, спробуйте ввести дані ще раз.");
             }
         }
 
-        float[][] a = new float[n][n];
-        float[][] b = new float[n][n];
-        float[][] c = new float[n][n];
+        float[][] a = new float[n][m];
+        float[][] b = new float[n2][m2];
+        float[][] c = new float[n][m2];
 
+        //заповнення матриць рандомними числами
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                a[i][j] = (float) Math.round(rand.nextFloat()*10);
-                b[i][j] = (float) Math.round(rand.nextFloat()*10);
+            for (int j = 0; j < m; j++) {
+                a[i][j] = rand.nextFloat() * 20;
             }
         }
+        for (int i = 0; i < n2; i++) {
+            for (int j = 0; j < m2; j++) {
+                b[i][j] = rand.nextFloat() * 20;
+            }
+        }
+
         System.out.println("---------------------"+"\n"+"Отримані матриці:"+"\n"+"Матриця А:");
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m; j++) {
                 System.out.printf("%5.1f", a[i][j]);
             }
             System.out.println("");
         }
         System.out.println("Матриця B:");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < n2; i++) {
+            for (int j = 0; j < m2; j++) {
                 System.out.printf("%5.1f", b[i][j]);
             }
             System.out.println("");
         }
-        //умножение
-        for (int i=0; i<n; i++){
-            for (int j=0; j<n; j++){
-                for (int k=0; k<n; k++) {
-                    c[i][j] += a[i][k] * b[k][j];
-                }}}
 
+        //множення матриць
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m2; j++) {
+                for (int k = 0; k < m; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        //Сума найменших елементів у непарних стовбцях і найбільших у парних
         float S=0;
         float max = 0;
         float min = c[0][0];
-        for (int j = 0; j < n; j++) {
-            if ((j+1) % 2 == 1) {
-                for (int i = 0; i < n; i++) {
-                    if (c[i][j]<=min) min=c[i][j];
+        for (int j = 0; j < m2; j++) {
+            if ((j+1) % 2 == 0) {
+                for (int i = 0; i < m; i++) {
+                    if (c[i][j] > max) {
+                        max = c[i][j];
+                    }
                 }
-                S+=min;
-            }
-            else {
-                for (int i = 0; i < n; i++) {
-                    if (c[i][j]>=max) max=c[i][j];
+                S += max;
+                max = 0;
+            } else {
+                for (int i = 0; i < m; i++) {
+                    if (c[i][j] < min) {
+                        min = c[i][j];
+                    }
                 }
-                S+=max;
+                S += min;
+                min = c[0][0];
             }
-            min=c[0][0];
-            max=0;
         }
-
 
         System.out.println("---------------------"+"\n"+"Результат:"+"\n"+"Матриця С=A×B:");
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < m2; j++) {
                 System.out.printf("%6.1f", c[i][j]);
             }
             System.out.println("");
